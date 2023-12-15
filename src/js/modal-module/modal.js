274cb7492f6e725117743346c1877ebcd1423662
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-12-15 10:35:57 +0100

    US6 - Implements Modal Class with Settings Modal returns Promises instead of value, let user decides to use Promise or not

---
+++ src/js/modal-module/modal.js
@@ -0,0 +1,28 @@
+/**
+ * modal.js
+ *  Class that make and load a modal
+ */
+import { Setting } from "./settings";
+export class Modal {
+    /**
+     * Set modal settings
+     * @see Setting class
+     * @param {Setting} settings 
+     * @returns void
+     */
+    setSettings(settings) {}
+
+    /**
+     * Loads a modal
+     * @param void
+     * @returns Promise<any>
+     */
+    load() {
+    }
+
+    /**
+     * Close the modal
+     * @returns Promise<any>
+     */
+    close() {}
+}
