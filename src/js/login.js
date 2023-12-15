5bf9f02a570efc611cfaf4e543387c62a6ba498d
Florian REY <florianrey.info@gmail.com>
2023-12-15 10:42:50 +0100

    ADD (US-1) | Add login page

---
+++ src/js/login.js
@@ -0,0 +1,4 @@
+const loginForm = document.getElementById("login-form");
+const emailInput = document.getElementById("email-input");
+const emailError = document.querySelector("#email-input + span.error");
+const passwordInput = document.getElementById("password-input");
